6a2232047cca73599f507331d62828e7d4dd0143
Ryan Dalton <rdalton2405@gmail.com>
2026-07-01 16:41:58 -0400

    updatequeue for new actor classes

--- src/src/updatequeue.cpp
+++ src/src/updatequeue.cpp
@@ -1,2 +1,31 @@
-void update() {
-}
+#include "bullet.h"
+#include "enemy.h"
+#include "player.h"
+#include <vector>
+
+class Game {
+public:
+	void update() {
+		for (auto &player : players) {
+		}
+	}
+
+    void insert_player(){
+
+    }
+
+    void insert_enemy(){
+
+    }
+    
+    void insert_bullets(){
+
+    }
+
+private:
+	std::vector<Player *> players;
+	std::vector<Enemy *> enemies;
+	std::vector<Bullet *> bullets;
+
+
+};
