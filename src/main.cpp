a8ea0eef2f71afeca7084da0316672fe4918b77a
Ryan Dalton <rdalton2405@gmail.com>
2026-07-01 16:39:36 -0400

    cleanup on aisle main

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,6 @@ int main(int argc, char *argv[]) {
 	SDL_Event event;
 	bool running = true;
 	Uint64 LAST = SDL_GetPerformanceCounter();
-	// like all of this needs to be moved over to sprite factories and abstracted spaces eventually but not now
 
 	backgroundManager = bgManager::getInstance();
 	backgroundManager->moonSceneInit();
@@ -52,10 +51,10 @@ int main(int argc, char *argv[]) {
 		const Uint64 frameStart = SDL_GetPerformanceCounter();
 		const Uint64 now = frameStart;
 		dt = (double)((now - LAST) * 1000 / (double)SDL_GetPerformanceFrequency());
-		LAST = now; // deltatime calculations
+		LAST = now;
 		SDL_GetKeyboardState(NULL);
 
-		while (SDL_PollEvent(&event)) { // event handler
+		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_EVENT_QUIT) {
 				running = false;
 			}
@@ -69,20 +68,9 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		/*
-			Bounds Checking
-		*/
 		_update();
-		// update & run simulations
-		// hudMgr->HudUpdate();
-		//  render everything that occured within the frame
-		//  wayland requires that something must be drawn to the screen in order for the window to actually exist i spent 2 hours figuring this out
-
 		_framesetup();
 
-	// SDL_RenderTexture(renderer, texture, NULL, &dst_rect);
-
-	// gotolabel to implement pausing
 	renderPresent:
 		_draw();
 
@@ -93,7 +81,6 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	// cleanup
 	SDL_DestroyRenderer(g_renderer);
 	SDL_DestroyWindow(g_window);
 	SDL_Quit();
@@ -102,40 +89,26 @@ int main(int argc, char *argv[]) {
 }
 
 void _framesetup() {
-	SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); // bg color
-	SDL_RenderClear(g_renderer);								   // clear canvas
+	SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); 
+	SDL_RenderClear(g_renderer);								   
 }
 
 void _draw() {
 	if (player.m_dst.y < 64) {
 		hudMgr->makeTranslucent();
-	} // bounds for when player is hidden behind UI elements
+	}
 
 	if (player.m_dst.y > 64) {
 		hudMgr->makeOpaque();
-	} // bounds for when player isnt hidden behind ui elements
+	}
 
 	backgroundManager->moonSceneRender();
 	hudMgr->HudRender();
 
-	// player and enemies and things like that
 	player.Render();
 	bul.Render();
-	// hud things and whatever else that should be drawn over the player or maybe we swap it i really dont know atm i cant lie
-	//		HPLV.Render();
-	//		gunIcon.Render();
-	//		levelIcon.Render();
-	// render everything that occured within the frame
-	// wayland requires that something must be drawn to the screen in order for the window to actually exist i spent 2 hours figuring this out
-	// SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); // bg color
-	// SDL_RenderClear(renderer);									 // clear canvas
 
-	// SDL_RenderTexture(renderer, texture, NULL, &dst_rect);
-	//
-	// SDL_RenderPresent(renderer);
-
-	SDL_RenderPresent(g_renderer); // put it all together now
-}
+	SDL_RenderPresent(g_renderer);
 
 void _update() {
 	player.Update(dt, bul);
@@ -145,13 +118,6 @@ void _update() {
 }
 
 void startgame() {
-	// set game's mode
 	mode = GAME;
-	// init character info
-	// playercharacter;
-	// init stats
-	// reset background stuff
-	// reset bullets vector
-	// reset enemies vector
-	//
+
 }
