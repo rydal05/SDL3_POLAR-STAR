7b7dc5fc6193c3c9fc4d194f883a41d7663c80a5
Ryan Dalton <rdalton2405@gmail.com>
2026-07-02 18:10:38 -0400

    misc

---
+++ src/src/main.cpp
@@ -0,0 +1,121 @@
+// my awesome 2d game engine framework thing
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <SDL3/SDL_scancode.h>
+#include <math.h>
+#include <stdio.h>
+
+#include "BackgroundManager.h"
+#include "Bullet.h"
+#include "GameDefs.h"
+#include "HudManager.h"
+#include "Player.h"
+#include "SDLApplication.h"
+
+void _draw();
+void _update();
+void _init();
+void _framesetup();
+
+bgManager *backgroundManager = nullptr;
+HudManager *hudMgr = nullptr;
+double dt = 0.0;
+bool paused = false;
+
+bgManager *bgManager::instancePtr = nullptr;
+std::mutex bgManager::mtx;
+
+HudManager *HudManager::instancePtr = nullptr;
+std::mutex HudManager::mtx;
+
+int main(int argc, char *argv[]) {
+	// setup
+
+	SDLApplication app("POLAR STAR", 640, 480);
+	const double targetFrameMs = 1000.0 / 60.0;
+
+	SDL_Event event;
+	bool running = true;
+	Uint64 LAST = SDL_GetPerformanceCounter();
+
+	backgroundManager = bgManager::getInstance();
+	backgroundManager->moonSceneInit();
+
+	hudMgr = HudManager::getInstance();
+	hudMgr->gameplayHudInit();
+
+	while (running) {
+		const Uint64 frameStart = SDL_GetPerformanceCounter();
+		const Uint64 now = frameStart;
+		dt = (double)((now - LAST) * 1000 / (double)SDL_GetPerformanceFrequency());
+		LAST = now;
+		SDL_GetKeyboardState(NULL);
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				running = false;
+			}
+			if (event.type == SDL_EVENT_KEY_DOWN) {
+				if (event.key.scancode == SDL_SCANCODE_ESCAPE) {
+					running = false;
+				}
+				if (event.key.scancode == SDL_SCANCODE_P) {
+					paused = !paused;
+				}
+			}
+		}
+
+		_update();
+		_framesetup();
+
+	renderPresent:
+		_draw();
+
+		const Uint64 frameEnd = SDL_GetPerformanceCounter();
+		const double frameMs = (double)((frameEnd - frameStart) * 1000 / (double)SDL_GetPerformanceFrequency());
+		if (frameMs < targetFrameMs) {
+			SDL_Delay((Uint32)(targetFrameMs - frameMs));
+		}
+	}
+
+	SDL_DestroyRenderer(GameDefs::g_renderer);
+	SDL_DestroyWindow(GameDefs::g_window);
+	SDL_Quit();
+
+	return 0;
+}
+
+void _framesetup() {
+	SDL_SetRenderDrawColor(GameDefs::g_renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(GameDefs::g_renderer);
+}
+
+void _draw() {
+	// if (player.m_dst.y < 64) {
+	// 	hudMgr->makeTranslucent();
+	// }
+
+	// if (player.m_dst.y > 64) {
+	// 	hudMgr->makeOpaque();
+	// }
+
+	backgroundManager->moonSceneRender();
+	hudMgr->HudRender();
+
+	// update_queue();
+	// render_queue();
+
+	SDL_RenderPresent(GameDefs::g_renderer);
+}
+
+void _update() {
+	// player.Update(dt, bul);
+	// bul.Update(dt);
+
+	backgroundManager->moonSceneUpdate(dt);
+}
+
+void startgame() {
+	// mode = GAME;
+}
