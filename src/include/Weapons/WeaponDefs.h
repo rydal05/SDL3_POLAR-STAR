4f28fe2ae02b15fe2ac3e617d7711513719d2f25
Ryan Dalton <rdalton2405@gmail.com>
2026-07-16 16:38:31 -0400

    wepdefs header setup

--- src/include/Weapons/WeaponDefs.h
+++ src/include/Weapons/WeaponDefs.h
@@ -1,6 +1,7 @@
 #ifndef WEAPON_DEFS_H
 #define WEAPON_DEFS_H
 
+#include <cstdint>
 
 enum WeaponsList {
 	POLAR,
@@ -16,6 +17,30 @@ enum WeaponsList {
 	STAFF,
 
     LIST_SIZE
+};
+
+enum StatsOrder {
+    FIRE_RATE,
+    AMMO_MAX,
+    DAMAGE_LEVEL_ONE,
+    DAMAGE_LEVEL_TWO,
+    DAMAGE_LEVEL_THREE,
+    XP_REQUIRED_TWO,
+    XP_REQUIRED_THREE,
+    AMMO_PICKUP_SIZE,
+
+    STATS_SIZE
+};
+
+std::int8_t stats[STATS_SIZE][LIST_SIZE] = {
+    1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,
 };
 
 #endif
