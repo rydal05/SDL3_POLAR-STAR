d2c9eb323df685a168d06ff91010ef512e3166a2
Ryan Dalton <rdalton2405@gmail.com>
2026-07-16 16:31:58 -0400

    reworking base player class

--- src/include/Players/ActorPlayer.h
+++ src/include/Players/ActorPlayer.h
@@ -4,8 +4,12 @@
 #include "GameDefs.h"
 #include "Player.h"
 #include "Sprite.h"
+#include "Weapons/WeaponDefs.h"
 
+#include <cstdint>
 #include <SDL3/SDL_scancode.h>
+
+
 
 class ActorPlayer : public Player {
 public:
@@ -13,14 +17,21 @@ public:
 	void update(double dt) override;
 	void render() override;
 	void move(float x, float y);
+	
+	void movement(const bool *state, double dt);
+	void collision(const bool *state);
+	void swapWeapon(const bool *state);
+	void shoot(const bool *state);
 
-private:
+protected:
 	Sprite m_sprite;
 	SDL_FRect m_dst;
 	SDL_Event *event;
 
 	SDL_Gamepad *m_gamepad;
 	float SPEED = 0.250f;
+
+	WeaponsList weapons[POLAR];	
 };
 
 #endif
--- src/src/ActorPlayer.cpp
+++ src/src/ActorPlayer.cpp
@@ -7,6 +7,12 @@ ActorPlayer::ActorPlayer() : m_sprite("assets/img/stg_story.bmp") {
 	m_dst.w = 16 * GameDefs::ScaleFactor;
 	m_dst.h = 16 * GameDefs::ScaleFactor;
 
+
+	// ensure parity with sprite
+
+	// SDL_Log("[ACTORPLAYER] update finished");
+
+	
 	m_sprite.Draw_Src(0, 0, 16, 16);
 	m_sprite.Draw_Dst(m_dst.x, m_dst.y);
 	m_sprite.Draw_Siz(m_dst.w, m_dst.h);
@@ -18,6 +24,38 @@ ActorPlayer::ActorPlayer() : m_sprite("assets/img/stg_story.bmp") {
 
 void ActorPlayer::update(double dt) {
 	const bool *state = SDL_GetKeyboardState(nullptr);
+	SDL_PumpEvents();
+	movement(state, dt);
+	collision(state);
+	
+	/*
+	1. Handle inputs
+	2. Handle external interactions (pickups, contact with bullets, border collisions, etc)
+	3. Hud updates
+	...
+	n-1. Pair sprite to pos
+	*/
+}
+
+void ActorPlayer::render() {
+	if (m_dst.y < 64) {
+		Hud::getInstance().makeTranslucent();
+	} // bounds for when player is hidden behind UI elements
+	
+	if (m_dst.y > 64) {
+		Hud::getInstance().makeOpaque();
+	}
+	
+	m_sprite.m_dst = m_dst; // moved
+	m_sprite.Render();
+}
+
+void ActorPlayer::move(float x, float y) { // not movement just explicit placement at a location
+	m_dst.x += SPEED * x;
+	m_dst.y += SPEED * y;
+}
+
+void ActorPlayer::movement(const bool *state, double dt) {
 	if (GameDefs::joystick != NULL) {
 		int total, i;
 		total = SDL_GetNumJoystickHats(GameDefs::joystick);
@@ -38,50 +76,36 @@ void ActorPlayer::update(double dt) {
 			}
 		}
 	} else if (GameDefs::joystick == NULL) {
-		if (state[SDL_SCANCODE_RIGHT]) {
+		if (state[SDL_SCANCODE_RIGHT] || state[SDL_SCANCODE_D]) {
 			m_dst.x += static_cast<float>(SPEED * dt);
 			// SDL_Log("[ACTORPLAYER] Input received");
 		}
-		if (state[SDL_SCANCODE_LEFT]) {
+		if (state[SDL_SCANCODE_LEFT] || state[SDL_SCANCODE_A]) {
 			m_dst.x -= static_cast<float>(SPEED * dt);
 			// SDL_Log("[ACTORPLAYER] Input received");
 		}
-		if (state[SDL_SCANCODE_DOWN]) {
+		if (state[SDL_SCANCODE_DOWN] || state[SDL_SCANCODE_S]) {
 			m_dst.y += static_cast<float>(SPEED * dt);
 			// SDL_Log("[ACTORPLAYER] Input received");
 		}
-		if (state[SDL_SCANCODE_UP]) {
+		if (state[SDL_SCANCODE_UP] || state[SDL_SCANCODE_W]) {
 			m_dst.y -= static_cast<float>(SPEED * dt);
 			// SDL_Log("[ACTORPLAYER] Input received");
 		}
 	}
+}
 
+void ActorPlayer::collision(const bool *state) {
 	if (m_dst.y > 448.0f) m_dst.y = 448.0f;
 	if (m_dst.x > 608.0f) m_dst.x = 608.0f;
 	if (m_dst.y < 0.0f) m_dst.y = 0.0f;
 	if (m_dst.x < 0.0f) m_dst.x = 0.0f;
-
-	if (m_dst.y < 64) {
-		Hud::getInstance().makeTranslucent();
-	} // bounds for when player is hidden behind UI elements
-
-	if (m_dst.y > 64) {
-		Hud::getInstance().makeOpaque();
-	}
-
-	// ensure parity with sprite
-	m_sprite.m_dst = m_dst;
-
-	// SDL_Log("OBJ SPRITE DIFF: %.2f %.2f %.2f %.2f", m_dst.x - m_sprite.m_dst.x, m_dst.y - m_sprite.m_dst.y, m_dst.w - m_sprite.m_dst.w, m_dst.h - m_sprite.m_dst.h);
-
-	// SDL_Log("[ACTORPLAYER] update finished");
 }
 
-void ActorPlayer::render() {
-	m_sprite.Render();
+void ActorPlayer::swapWeapon(const bool *state) {
+
 }
 
-void ActorPlayer::move(float x, float y) {
-	m_dst.x += SPEED * x;
-	m_dst.y += SPEED * y;
+void ActorPlayer::shoot(const bool *state) {
+
 }
