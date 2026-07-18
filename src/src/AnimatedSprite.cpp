b9b9b18562ce7f44d756f5b72ddb900b3384b4d6
Ryan Dalton <rdalton2405@gmail.com>
2026-07-18 00:09:54 -0400

    Animatedsprite loose definitions

---
+++ src/src/AnimatedSprite.cpp
@@ -0,0 +1,34 @@
+#include <SDL3/SDL.h>
+#include <string>
+
+#include "Sprite.h"
+#include "Managers/ResourceManager.h"
+#include "GameDefs.h"
+
+AnimatedSprite::AnimatedSprite(std::string filepath, int frames){
+    char *new_filepath = NULL;
+	SDL_asprintf(&new_filepath, "%s%s", SDL_GetBasePath(), filepath);
+	SDL_Surface *retrieveSurface = ResourceManager::GetInstance().GetSurface(new_filepath);
+	m_texture = SDL_CreateTextureFromSurface(GameDefs::g_renderer, retrieveSurface);
+	SDL_free(new_filepath);
+
+    this->frames = frames;
+}
+
+
+
+void AnimatedSprite::Draw(int x, int y, int w, int h){
+
+}
+
+void AnimatedSprite::PlayFrame(int x, int y, int w, int h, int frame){
+    
+}
+
+void AnimatedSprite::Update(){
+
+}
+
+void AnimatedSprite::Render(){
+
+}
