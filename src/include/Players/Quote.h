ccda8f42a40e22702546eb6ea4bc2c4f9398815c
Ryan Dalton <rdalton2405@gmail.com>
2026-07-26 15:25:10 -0400

    quote class updating

--- src/include/Players/Quote.h
+++ src/include/Players/Quote.h
@@ -7,3 +7,19 @@ polar star, missile launcher, bubbline, snake (sidegrade from polar star), spur 
 
 
 */
+
+#include "ActorPlayer.h"
+#include "Weapons/WeaponDefs.h"
+
+class Quote : public ActorPlayer {
+public:
+	Quote();
+    ~Quote();
+
+
+
+private:
+    Sprite *m_sprite = new Sprite("assets/img/stg_story.bmp");
+
+    
+};
