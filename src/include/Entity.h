afc41d3a5ae347bf345a0bd90893f73a4fe293fe
Ryan Dalton <rdalton2405@gmail.com>
2026-07-28 13:03:14 -0400

    Sprites are now managed at the entity level.
    All derviatives of entity contain the basis for sprite definitions to everything is all uniform, grabbing textures for texture manipulation (transparency, GPU effects, etc) is also standardized.

--- src/include/Entity.h
+++ src/include/Entity.h
@@ -1,11 +1,21 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include "Sprite.h"
+
 class Entity {
 public:
 	virtual void Update(double dt) = 0;
 	virtual void Render() = 0;
 	virtual ~Entity() {}
+
+	SDL_Texture *m_GetTexture() {
+		return m_sprite->GetTexture();
+	}
+
+protected:
+	Entity(const char* path) : m_sprite(new Sprite(path)) {}
+	Sprite *m_sprite;
 };
 
 #endif
