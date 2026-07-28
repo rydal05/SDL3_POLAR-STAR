b5d577e0b58ce3bcc61f22f8a3223d0f99eaac76
Ryan Dalton <rdalton2405@gmail.com>
2026-07-27 20:40:45 -0400

    commented out  resourcemgr logs

--- src/src/Managers/ResourceManager.cpp
+++ src/src/Managers/ResourceManager.cpp
@@ -17,14 +17,14 @@ ResourceManager &ResourceManager::GetInstance() {
 }
 
 SDL_Surface *ResourceManager::GetSurface(std::string filepath) {
-	std::cout << "Filepath given: " << filepath << std::endl;
+	// std::cout << "Filepath given: " << filepath << std::endl;
 
 	auto search = m_surfaces.find(filepath);
 	if (search != m_surfaces.end()) {
-		std::cout << "Given path was successfuly retrieved." << std::endl;
+		// std::cout << "Given path was successfuly retrieved." << std::endl;
 		return m_surfaces[filepath];
 	} else {
-		std::cout << "Given path was added to database." << std::endl;
+		// std::cout << "Given path was added to database." << std::endl;
 		SDL_Surface *surface = SDL_LoadBMP(filepath.c_str());
 		m_surfaces.insert(std::make_pair(filepath, surface));
 		return m_surfaces[filepath];
