54415953a7f5bb9c7e055e8e3b38a765608afbe5
Ryan Dalton <rdalton2405@gmail.com>
2026-08-11 15:04:14 -0400

    bullet superclass setup

--- src/include/superclasses/Bullet.h
+++ src/include/superclasses/Bullet.h
@@ -1,13 +1,32 @@
 #ifndef BULLET_H
 #define BULLET_H
 
+/*
+RESPONSIBILITIES OF A BULLET:
+
+1: Use math to determine path of travel using functions and whatever else
+2: Manage rendering style PER BULLET
+3: Maintain info that is pertinent per bullet such as damage, travel speed, etc.
+4: Handle and reconcile collisions
+5: Handle and reconcile ownership
+*/
+
 #include "superclasses/Entity.h"
 
 class Bullet : public Entity {
 public:
+	Bullet();
+	explicit Bullet(const char *filepath);
+
 	virtual void Update(double dt) = 0;
 	virtual void Render() = 0;
-	virtual ~Bullet() {}
+	
+	void move(float x, float y);
+
+	void travel();
+	bool collision();
+
+private:
 };
 
 #endif
