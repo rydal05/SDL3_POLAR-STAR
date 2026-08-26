bc9d6615c6b6b48923d9009fac3cb5434817ff57
Ryan Dalton <rdalton2405@gmail.com>
2026-08-26 10:12:30 -0400

    auto formatting

--- src/include/superclasses/Player.h
+++ src/include/superclasses/Player.h
@@ -2,9 +2,9 @@
 #define ACTORPLAYER_H
 
 #include "GameDefs.h"
-#include "superclasses/Sprite.h"
 #include "Weapons/WeaponDefs.h"
 #include "superclasses/Entity.h"
+#include "superclasses/Sprite.h"
 
 #include <SDL3/SDL_scancode.h>
 #include <cstdint>
@@ -12,11 +12,11 @@
 class ActorPlayer : public Entity {
 public:
 	ActorPlayer();
-	explicit ActorPlayer(const char* filepath);
+	explicit ActorPlayer(const char *filepath);
 
 	virtual void Update(double dt) override;
 	virtual void Render() override;
-	
+
 	void move(float x, float y);
 	void movement(const bool *state, double dt);
 	void inputs(const bool *state, double dt);
@@ -25,12 +25,11 @@ public:
 
 	void swapWeaponLeft(const bool *state);
 	void swapWeaponRight(const bool *state);
-	
+
 	void shoot(const bool *state);
 	void tapFire();
 	void chargeFire();
 	void rapidFire();
-
 
 protected:
 	SDL_FPoint coreOffset{8.0f, 10.0f};
@@ -38,7 +37,6 @@ protected:
 	int weapons[WEAPON_LIST_SIZE] = {0};
 	bool held;
 	std::uint16_t heldFrames;
-
 };
 
 #endif
