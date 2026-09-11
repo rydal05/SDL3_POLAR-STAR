edfff03c2901f7ff2f90abaf3760d3519ac61c91
Ryan Dalton <rdalton2405@gmail.com>
2026-09-11 07:58:12 -0400

    prior

--- src/include/superclasses/Weapon.h
+++ src/include/superclasses/Weapon.h
@@ -3,14 +3,14 @@
 
 #include "superclasses/Entity.h"
 
-class Weapon : public Sprite{ //weapons are a literal sprite class on top of being functional for actual bullet shooting, this just gives us a way to abstract away the physical gun appearance and rendering separate from needing a player sprite for that specific weapon
+class Weapon : public Sprite { // weapons are a literal sprite class on top of being functional for actual bullet shooting, this just gives us a way to abstract away the physical gun appearance and rendering separate from needing a player sprite for that specific weapon
 public:
 	Weapon();
 	explicit Weapon(const char *filepath);
 
 	virtual void Update(double dt) = 0; // update loop will interpret inputs and correspond weapon attack outputs
 	virtual void Render() = 0;
-	
+
 	virtual void Shoot() = 0;
 	// virtual void Hold() = 0;
 
