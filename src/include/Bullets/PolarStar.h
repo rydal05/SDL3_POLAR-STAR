f065d09e978e4f052580857793e1a01793c4a8fa
Ryan Dalton <rdalton2405@gmail.com>
2026-09-11 09:16:57 -0400

    weapons and bullets are now functional, offsets are calculated on a per weapon/bullet basis

--- src/include/Bullets/PolarStar.h
+++ src/include/Bullets/PolarStar.h
@@ -5,9 +5,12 @@ public:
 	Bul_PS(SDL_FRect c_dst, int level, int charge) : Bullet("assets/img/Polar_Star_Players.bmp") {
 		SDL_Log("Create bullet");
 
+		offset_X = c_dst.x + c_dst.w;
+		offset_Y = c_dst.y + (c_dst.h / 2);
+
 		m_sprite->Draw_Src(66, 5, 11, 4);
-		m_sprite->Draw_Dst(c_dst.x, c_dst.y);
-		m_sprite->Draw_Siz(16.0f, 16.0f);
+		m_sprite->Draw_Dst(offset_X, offset_Y);
+		m_sprite->Draw_Siz(11.0f, 4.0f);
 	}
 
 	virtual void Update(double dt) override {
@@ -23,4 +26,6 @@ private:
 	int damage = 1;
 	// bool playerOwnership = true;
 	int direction = 1;
+	float offset_X;
+	float offset_Y = 8.0f;
 };
--- src/include/Weapons/PolarStar.h
+++ src/include/Weapons/PolarStar.h
@@ -11,17 +11,20 @@
 class PolarStar : public Weapon {
 public:
 	PolarStar(Entity *callee) : Weapon("assets/img/Polar_Star_Players.bmp") {
-
 		this->owner = callee;
+
+		m_sprite->Draw_Src(51, 6, 9, 3);
+		m_sprite->Draw_Dst(owner->getSprite()->get_X() + offset_X, owner->getSprite()->get_Y() + offset_Y);
+		m_sprite->Draw_Siz(9, 3);
 		// ok so i can either explicitly state weapon stats in here but that FEELS intrinically wasteful like I don't know if it would be better to manage that entirely within the weapondefs header but in here it is a lot easier to look at specific stats for weapons when i want to rather than looking through a giant disgusting list
 	}
 	~PolarStar() {}
 
 	virtual void Update(double dt) override {
-		// doi nitgiung
+		m_sprite->Draw_Dst(owner->getSprite()->get_X() + offset_X, owner->getSprite()->get_Y() + offset_Y);
 	};
 	virtual void Render() override {
-		// do nothing
+		m_sprite->Render();
 	};
 
 	void Shoot() {
@@ -32,6 +35,8 @@ public:
 
 private:
 	float stats[(int)(weapondefs::STATS_SIZE)][4] = {0};
+	float offset_X = 12.0f;
+	float offset_Y = 9.0f;
 };
 
 #endif
